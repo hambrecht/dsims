4a393d5bd5dcf95de3ab71c0a9f87fc0bee68933
hambrecht <137965368+hambrecht@users.noreply.github.com>
2026-05-05 15:52:45 -0700

    perf(parallel): switch to worker-state load-balanced execution
    - initialize worker state once per cluster
    - dispatch only repetition indices to workers
    - replace static scheduling with parLapplyLB for better core utilization
    - remove repeated heavy argument passing in parallel loop

--- R/run.simulation.R
+++ R/run.simulation.R
@@ -21,7 +21,7 @@
 #' @return the \code{\link{Simulation-class}} object which now includes
 #' the results
 #' @export
-#' @importFrom parallel detectCores makeCluster clusterEvalQ stopCluster parLapply
+#' @importFrom parallel detectCores makeCluster clusterEvalQ stopCluster parLapplyLB clusterExport
 #' @importFrom rstudioapi versionInfo
 #' @rdname run.simulation-methods
 #' @seealso \code{\link{make.simulation}}
@@ -64,62 +64,172 @@ check.parallel.capability <- function(run.parallel, max.cores = NA) {
 run.simulation <- function(simulation, run.parallel = FALSE, max.cores = NA, 
                          counter = TRUE, transect.path = character(0), 
                          progress.file = character(0)) {
-  # Input validation
-  if (!inherits(simulation, "Simulation")) {
-    stop("Input must be a Simulation-class object")
-  }
-  
-  # Initialize variables
   save.data <- load.data <- FALSE
   data.path <- character()
-  
-  # Process transect path
-  if (length(transect.path) > 0) {
-    transect.path <- process.transect.path(transect.path, simulation@reps)
-  }
-  
-  # Reset results and warnings
-  simulation@results <- create.results.arrays(simulation@reps,
-                                            simulation@design@region,
-                                            simulation@ds.analysis,
-                                            simulation@population.description)
-  simulation@warnings <- list(message = list(),
-                            counter = list(),
-                            index = list())
-  
-  # Check parallel capabilities
-  run.parallel <- check.parallel.capability(run.parallel, max.cores)
-  
-  # Main execution
-  if (run.parallel) {
-    # Initialize cluster
-    myCluster <- parallel::makeCluster(nCores)
-    on.exit(stopCluster(myCluster))
-    
-    parallel::clusterEvalQ(myCluster, {
-      require(dsims)
-      gc()
-    })
-    
-    # Run parallel simulation
-    simulation <- run.parallel.simulation(simulation, myCluster, counter,
-                                        save.data, load.data, data.path,
-                                        transect.path, progress.file)
-    
-  } else {
-    # Run serial simulation
-    simulation <- run.serial.simulation(simulation, counter,
-                                      save.data, load.data, data.path,
-                                      transect.path, progress.file)
-  }
-  
-  # Display accumulated warnings
-  if (length(simulation@warnings$message) > 0) {
-    for(w in seq_along(simulation@warnings$message)){
-      warning(simulation@warnings$message[[w]], immediate. = TRUE, call. = FALSE)
+  # Check if the transect path ends in / if so remove - hard to do the same for windows!
+  if(length(transect.path) > 0){
+    if(substr(transect.path, nchar(transect.path), nchar(transect.path)) == "/"){
+      transect.path <- substr(transect.path, 0, nchar(transect.path)-1)
     }
   }
-  
+  transect.path.master <- transect.path
+  # Check if it is a single transect set or a folder
+  if(length(transect.path) > 0){
+    # Check if a folder or file have been specified
+    check.path <- unlist(strsplit(transect.path, split = "[.]"))
+    index <- length(check.path)
+    if(check.path[index] == "shp"){
+      # Set transect path to repeat the same value for each rep (for running in parallel)
+      transect.path <- rep(transect.path, simulation@reps)
+    }else{
+      # Make up the vector of shapefile paths
+      transect.path <- file.path(transect.path, list.files(path = transect.path, pattern = "shp"))
+      if(length(transect.path) == 0){
+        stop(paste("No shapefiles found at ", transect.path.master,", cannot run simulation.", sep = ""), call. = FALSE)
+      }
+      if(length(transect.path) < simulation@reps){
+        warning(paste("Insufficient transect shapefiles supplied (", length(transect.path)," supplied, ", simulation@reps, " required). Simulation will use some sets of transects more than once, this may influence the results.", sep = ""), immediate. = TRUE, call. = FALSE)
+        mult.factor <- ceiling(simulation@reps/length(transect.path))
+        transect.path <- rep(transect.path, mult.factor)[1:simulation@reps]
+      }
+    }
+  }
+  simulation@results <- create.results.arrays(simulation@reps,
+                                              simulation@design@region,
+                                              simulation@ds.analysis,
+                                              simulation@population.description)
+  #reset the error/warning message
+  simulation@warnings$message <- list()
+  simulation@warnings$counter <- list()
+  simulation@warnings$index <- list()
+  #check the data.path ends in "/"
+  if(length(data.path) > 0){
+    temp.path <- strsplit(data.path, split = "")
+    if(temp.path[length(temp.path)] != "/"){
+      # if not add it
+      data.path <- paste(data.path, "/", sep = "")
+    }
+    rm(temp.path)
+  }
+  # Two if(run.parallel) checks as if libraries not present will change to
+  # false before running in parallel and then run in serial
+  if(run.parallel){
+    if(!requireNamespace('parallel', quietly = TRUE)){
+      warning("Could not run in parallel, check parallel library is installed.", immediate. = TRUE, call. = FALSE)
+      run.parallel = FALSE
+    }else{
+      # counts the number of cores you have
+      nCores <- getOption("cl.cores", detectCores()) - 1
+      if(!is.na(max.cores)){
+        nCores <- min(nCores, max.cores)
+      }
+      if(nCores <= 1){
+        warning("Could not run in parallel only one core available/requested (dsims limits running in parallel to 1 less than the number of cores on the machine).", immediate. = TRUE, call. = FALSE)
+        run.parallel = FALSE
+      }
+    }
+  }
+  if(run.parallel){
+    # there is an issue with parallel on some machines for some versions of R and R studio
+    ## WORKAROUND: https://github.com/rstudio/rstudio/issues/6692
+    ## Revert to 'sequential' setup of PSOCK cluster in RStudio v1.3.959 or lower on macOS with R v4.0.0 or higher
+    if (Sys.getenv("RSTUDIO") == "1" && !nzchar(Sys.getenv("RSTUDIO_TERM")) &&
+        Sys.info()["sysname"] == "Darwin" && getRversion() >= "4.0.0") {
+      if(rstudioapi::versionInfo()$version < "1.3.1056"){
+        #warning(paste("The combination of versions of R-studio and R you are using on your mac may lead to an error when running in parallel. Please run the following command to try to correct this before running the simulation again (alternatively try updating R-studio): parallel:::setDefaultClusterOptions(setup_strategy = \"sequential\")", sep = ""), immediate. = TRUE, call. = FALSE)
+        eval(parse(text = "parallel:::setDefaultClusterOptions(setup_strategy = \"sequential\")"))
+      }
+    }
+    # intitialise the cluster
+    myCluster <- parallel::makeCluster(nCores)
+    parallel::clusterEvalQ(myCluster, {
+      require(dsims)
+    })
+    worker.state <- list(simulation = simulation,
+                         save.data = save.data,
+                         load.data = load.data,
+                         data.path = data.path,
+                         transect.path = transect.path,
+                         save.transects = FALSE,
+                         progress.file = progress.file)
+    worker.fun <- function(i){
+      state <- get(".dsims_worker_state", envir = .GlobalEnv)
+      single.sim.loop(i = i,
+                      simulation = state$simulation,
+                      save.data = state$save.data,
+                      load.data = state$load.data,
+                      data.path = state$data.path,
+                      counter = FALSE,
+                      in.parallel = TRUE,
+                      transect.path = state$transect.path,
+                      save.transects = state$save.transects,
+                      progress.file = state$progress.file)
+    }
+    parallel::clusterExport(myCluster,
+                            varlist = c("worker.state", "worker.fun"),
+                            envir = environment())
+    parallel::clusterEvalQ(myCluster, {
+      .dsims_worker_state <- worker.state
+      NULL
+    })
+    on.exit(stopCluster(myCluster))
+    if(counter){
+      message("Parallel run uses load-balanced scheduling; per-repetition progress bar is disabled.")
+    }
+    results <- parallel::parLapplyLB(myCluster,
+                                     X = as.list(1:simulation@reps),
+                                     fun = worker.fun)
+    #Extract results and warnings
+    sim.results <- sim.warnings <- list()
+    for(i in seq(along = results)){
+      sim.results[[i]] <- results[[i]]$results
+      sim.warnings[[i]] <- results[[i]]$warnings
+    }
+    simulation <- accumulate.PP.results(simulation = simulation, results = sim.results)
+    simulation@warnings <- accumulate.warnings(sim.warnings)
+    stopCluster(myCluster)
+    on.exit()
+  }
+  if(!run.parallel){
+    #otherwise loop
+    sim.warnings <- vector("list", simulation@reps)
+    for(i in 1:simulation@reps){
+      results <- single.sim.loop(i = i,
+                                 simulation = simulation,
+                                 save.data = save.data,
+                                 load.data = load.data,
+                                 data.path = data.path,
+                                 counter = counter,
+                                 transect.path = transect.path,
+                                 save.transects = FALSE,
+                                 progress.file = progress.file)
+      if(!is.null(results$rep.result)){
+        simulation@results <- apply.rep.result(simulation@results,
+                                               results$rep.result,
+                                               i)
+        if(!is.null(results$filename) && length(results$filename) > 0){
+          simulation@results$filename[i] <- results$filename
+        }
+      }else{
+        simulation@results <- results$results
+      }
+      sim.warnings[[i]] <- results$warnings
+    }
+    simulation@warnings <- accumulate.warnings(sim.warnings)
+  }
+  simulation@results <- add.summary.results(results = simulation@results,
+                                            model.count = length(simulation@ds.analysis@dfmodel))
+  #Process warnings
+  if(length(simulation@warnings$message) > 0){
+    message("Summary of warnings and errors:")
+    for(i in seq(along = simulation@warnings$message)){
+      rep.info <- ifelse(is.null(simulation@warnings$index), "",
+                         paste(" in repetition(s): ", paste(simulation@warnings$index[[i]], collapse = ", ")))
+      message(paste(simulation@warnings$message[[i]], " (occurred ", simulation@warnings$counter[[i]], " time(s)", rep.info, ")", sep = ""))
+    }
+    message("-----")
+  }
+
   return(simulation)
 }
 
